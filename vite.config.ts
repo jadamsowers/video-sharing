e0d018da8871e363754c23e379c3fabf4b0fabe4
Adam Sowers <adam.sowers@gmail.com>
2026-04-30 16:24:17 -0400

    refactor: remove unused spawn import from vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, type ResolvedConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'node:fs'
 import path from 'node:path'
-import { spawn } from 'node:child_process'
+
 
 
 
